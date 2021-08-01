39f313807f684f7f77fffc150716c75e5ad763cf
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-07-31 20:50:03 -0500

    Not Needed

--- bills.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `bills` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(255) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `amount` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `citizenid` (`citizenid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1111 DEFAULT CHARSET=utf8mb4;
