40c50f649d6754c2ada9a80f69342fec90500aec
Developer-Bear <collindudra@gmail.com>
2021-08-05 11:39:58 -0500

    fix config to all grades by default

--- config.lua
+++ config.lua
@@ -251,7 +251,7 @@ Config.Items = {
             },
             type = "weapon",
             slot = 1,
-            authorizedJobGrades = {0}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [2] = {
             name = "weapon_stungun",
@@ -262,7 +262,7 @@ Config.Items = {
             },
             type = "weapon",
             slot = 2,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [3] = {
             name = "weapon_pumpshotgun",
@@ -276,7 +276,7 @@ Config.Items = {
             },
             type = "weapon",
             slot = 3,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [4] = {
             name = "weapon_smg",
@@ -306,7 +306,7 @@ Config.Items = {
             },
             type = "weapon",
             slot = 5,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [6] = {
             name = "weapon_nightstick",
@@ -315,7 +315,7 @@ Config.Items = {
             info = {},
             type = "weapon",
             slot = 6,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [7] = {
             name = "pistol_ammo",
@@ -333,7 +333,7 @@ Config.Items = {
             info = {},
             type = "item",
             slot = 8,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [9] = {
             name = "shotgun_ammo",
@@ -342,7 +342,7 @@ Config.Items = {
             info = {},
             type = "item",
             slot = 9,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [10] = {
             name = "rifle_ammo",
@@ -351,7 +351,7 @@ Config.Items = {
             info = {},
             type = "item",
             slot = 10,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [11] = {
             name = "handcuffs",
@@ -360,7 +360,7 @@ Config.Items = {
             info = {},
             type = "item",
             slot = 11,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [12] = {
             name = "weapon_flashlight",
@@ -369,7 +369,7 @@ Config.Items = {
             info = {},
             type = "weapon",
             slot = 12,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [13] = {
             name = "empty_evidence_bag",
@@ -378,7 +378,7 @@ Config.Items = {
             info = {},
             type = "item",
             slot = 13,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [14] = {
             name = "police_stormram",
@@ -387,7 +387,7 @@ Config.Items = {
             info = {},
             type = "item",
             slot = 14,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [15] = {
             name = "armor",
@@ -396,7 +396,7 @@ Config.Items = {
             info = {},
             type = "item",
             slot = 15,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [16] = {
             name = "radio",
@@ -405,7 +405,7 @@ Config.Items = {
             info = {},
             type = "item",
             slot = 16,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         },
         [17] = {
             name = "heavyarmor",
@@ -414,7 +414,7 @@ Config.Items = {
             info = {},
             type = "item",
             slot = 17,
-            authorizedJobGrades = {1, 2, 3, 4}
+            authorizedJobGrades = {0, 1, 2, 3, 4}
         }
     }
 }
