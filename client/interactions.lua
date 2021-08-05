f51dd78a3f5c6e1e96a0c5146cc7a9e443b8c0f1
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-05 11:10:36 +0300

    add multi check for player

--- client/interactions.lua
+++ client/interactions.lua
@@ -124,6 +124,38 @@ AddEventHandler('police:client:SeizeDriverLicense', function()
     end
 end)
 
+RegisterNetEvent('police:client:checkBank')
+AddEventHandler('police:client:checkBank', function()
+    local player, distance = GetClosestPlayer()
+    if player ~= -1 and distance < 2.5 then
+        local playerId = GetPlayerServerId(player)
+        TriggerServerEvent("police:server:checkBank", playerId)
+    else
+        QBCore.Functions.Notify("No one around!", "error")
+    end
+end)
+
+RegisterNetEvent('police:client:checkLicenses')
+AddEventHandler('police:client:checkLicenses', function()
+    local player, distance = GetClosestPlayer()
+    if player ~= -1 and distance < 2.5 then
+        local playerId = GetPlayerServerId(player)
+        TriggerServerEvent("police:server:checkLicenses", playerId)
+    else
+        QBCore.Functions.Notify("No one around!", "error")
+    end
+end)
+
+RegisterNetEvent('police:client:checkFines')
+AddEventHandler('police:client:checkFines', function()
+    local player, distance = GetClosestPlayer()
+    if player ~= -1 and distance < 2.5 then
+        local playerId = GetPlayerServerId(player)
+        TriggerServerEvent("police:server:checkFines", playerId)
+    else
+        QBCore.Functions.Notify("No one around!", "error")
+    end
+end)
 
 RegisterNetEvent('police:client:RobPlayer')
 AddEventHandler('police:client:RobPlayer', function()
