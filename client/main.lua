52de2ff2eb41c380bf3f26d86aab43d7dc4d0748
GhzGarage <gigahertzgarage@mail.com>
2021-09-13 20:35:05 -0500

    Global variables

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,11 @@
-local isLoggedIn = true
-local isHandcuffed = false
-local cuffType = 1
-local isEscorted = false
-local draggerId = 0
-local PlayerJob = {}
-local onDuty = false
-local databankOpen = false
+isLoggedIn = true
+isHandcuffed = false
+cuffType = 1
+isEscorted = false
+draggerId = 0
+PlayerJob = {}
+onDuty = false
+databankOpen = false
 
 Citizen.CreateThread(function()
     for k, station in pairs(Config.Locations["stations"]) do
@@ -32,7 +32,7 @@ AddEventHandler('QBCore:Client:OnJobUpdate', function(JobInfo)
     end
 
     if (PlayerJob ~= nil) and PlayerJob.name ~= "police" then
-        if DutyBlips ~= nil then 
+        if DutyBlips ~= nil then
             for k, v in pairs(DutyBlips) do
                 RemoveBlip(v)
             end
@@ -53,15 +53,29 @@ AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
     TriggerServerEvent("police:server:UpdateCurrentCops")
 
     if QBCore.Functions.GetPlayerData().metadata["tracker"] then
-        local trackerClothingData = {outfitData = {["accessory"] = { item = 13, texture = 0}}}
+        local trackerClothingData = {
+            outfitData = {
+                ["accessory"] = {
+                    item = 13,
+                    texture = 0
+                }
+            }
+        }
         TriggerEvent('qb-clothing:client:loadOutfit', trackerClothingData)
     else
-        local trackerClothingData = {outfitData = {["accessory"]   = { item = -1, texture = 0}}}
+        local trackerClothingData = {
+            outfitData = {
+                ["accessory"] = {
+                    item = -1,
+                    texture = 0
+                }
+            }
+        }
         TriggerEvent('qb-clothing:client:loadOutfit', trackerClothingData)
     end
 
     if (PlayerJob ~= nil) and PlayerJob.name ~= "police" then
-        if DutyBlips ~= nil then 
+        if DutyBlips ~= nil then
             for k, v in pairs(DutyBlips) do
                 RemoveBlip(v)
             end
@@ -81,7 +95,9 @@ AddEventHandler('police:client:sendBillingMail', function(amount)
         TriggerServerEvent('qb-phone:server:sendNewMail', {
             sender = "Central Judicial Collection Agency",
             subject = "Debt collection",
-            message = "Dear " .. gender .. " " .. charinfo.lastname .. ",<br /><br />The Central Judicial Collection Agency (CJCA) charged the fines you received from the police.<br />There is <strong>$"..amount.."</strong> withdrawn from your account.<br /><br />Kind regards,<br />Mr. I.K. Graai",
+            message = "Dear " .. gender .. " " .. charinfo.lastname ..
+                ",<br /><br />The Central Judicial Collection Agency (CJCA) charged the fines you received from the police.<br />There is <strong>$" ..
+                amount .. "</strong> withdrawn from your account.<br /><br />Kind regards,<br />Mr. I.K. Graai",
             button = {}
         })
     end)
@@ -104,18 +120,20 @@ AddEventHandler('police:client:toggleDatabank', function()
         end
         tabletProp = CreateObject(tabletModel, 1.0, 1.0, 1.0, 1, 1, 0)
         AttachEntityToEntity(tabletProp, PlayerPedId(), bone, 0.03, 0.002, -0.0, 10.0, 160.0, 0.0, 1, 0, 0, 0, 2, 1)
-        TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@base", "base", 3.0, 3.0, -1, 49, 0, 0, 0, 0)
+        TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@base", "base", 3.0, 3.0, -1,
+            49, 0, 0, 0, 0)
         SetNuiFocus(true, true)
         SendNUIMessage({
-            type = "databank",
+            type = "databank"
         })
     else
         DetachEntity(tabletProp, true, true)
         DeleteObject(tabletProp)
-        TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@base", "exit", 3.0, 3.0, -1, 49, 0, 0, 0, 0)
+        TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@base", "exit", 3.0, 3.0, -1,
+            49, 0, 0, 0, 0)
         SetNuiFocus(false, false)
         SendNUIMessage({
-            type = "closedatabank",
+            type = "closedatabank"
         })
     end
 end)
@@ -125,7 +143,8 @@ RegisterNUICallback("closeDatabank", function(data, cb)
     DetachEntity(tabletProp, true, true)
     DeleteObject(tabletProp)
     SetNuiFocus(false, false)
-    TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@base", "exit", 3.0, 3.0, -1, 49, 0, 0, 0, 0)
+    TaskPlayAnim(PlayerPedId(), "amb@code_human_in_bus_passenger_idles@female@tablet@base", "exit", 3.0, 3.0, -1, 49, 0,
+        0, 0, 0)
 end)
 
 RegisterNetEvent('QBCore:Client:OnPlayerUnload')
@@ -139,7 +158,7 @@ AddEventHandler('QBCore:Client:OnPlayerUnload', function()
     onDuty = false
     ClearPedTasks(PlayerPedId())
     DetachEntity(PlayerPedId(), true, false)
-    if DutyBlips ~= nil then 
+    if DutyBlips ~= nil then
         for k, v in pairs(DutyBlips) do
             RemoveBlip(v)
         end
@@ -151,9 +170,10 @@ local DutyBlips = {}
 RegisterNetEvent('police:client:UpdateBlips')
 AddEventHandler('police:client:UpdateBlips', function(players)
 
-    if PlayerJob ~= nil and (PlayerJob.name == 'police' or PlayerJob.name == 'ambulance' or PlayerJob.name == 'doctor') and onDuty then
+    if PlayerJob ~= nil and (PlayerJob.name == 'police' or PlayerJob.name == 'ambulance' or PlayerJob.name == 'doctor') and
+        onDuty then
 
-        if DutyBlips ~= nil then 
+        if DutyBlips ~= nil then
             for k, v in pairs(DutyBlips) do
                 RemoveBlip(v)
             end
@@ -174,7 +194,7 @@ function CreateDutyBlips(playerId, playerLabel, playerJob, playerLocation)
     local blip = GetBlipFromEntity(ped)
 
     if not DoesBlipExist(blip) then
-        
+
         if NetworkIsPlayerActive(playerId) then
             blip = AddBlipForEntity(ped)
         else
@@ -193,12 +213,12 @@ function CreateDutyBlips(playerId, playerLabel, playerJob, playerLocation)
         BeginTextCommandSetBlipName('STRING')
         AddTextComponentString(playerLabel)
         EndTextCommandSetBlipName(blip)
-        
+
         table.insert(DutyBlips, blip)
     end
 
     if GetBlipFromEntity(PlayerPedId()) == blip then
-        --Ensure we remove our own blip.
+        -- Ensure we remove our own blip.
         RemoveBlip(blip)
     end
 end
@@ -206,11 +226,12 @@ end
 RegisterNetEvent('police:client:SendPoliceEmergencyAlert')
 AddEventHandler('police:client:SendPoliceEmergencyAlert', function()
     local pos = GetEntityCoords(PlayerPedId())
-    local s1, s2 = Citizen.InvokeNative(0x2EB41072B4C1E4C0, pos.x, pos.y, pos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt())
+    local s1, s2 = Citizen.InvokeNative(0x2EB41072B4C1E4C0, pos.x, pos.y, pos.z, Citizen.PointerValueInt(),
+        Citizen.PointerValueInt())
     local street1 = GetStreetNameFromHashKey(s1)
     local street2 = GetStreetNameFromHashKey(s2)
     local streetLabel = street1
-    if street2 ~= nil then 
+    if street2 ~= nil then
         streetLabel = streetLabel .. " " .. street2
     end
     local alertTitle = "Assistance colleague"
@@ -220,26 +241,28 @@ AddEventHandler('police:client:SendPoliceEmergencyAlert', function()
 
     local MyId = GetPlayerServerId(PlayerId())
 
-    TriggerServerEvent("police:server:SendPoliceEmergencyAlert", streetLabel, pos, QBCore.Functions.GetPlayerData().metadata["callsign"])
+    TriggerServerEvent("police:server:SendPoliceEmergencyAlert", streetLabel, pos,
+        QBCore.Functions.GetPlayerData().metadata["callsign"])
     TriggerServerEvent('qb-policealerts:server:AddPoliceAlert', {
         timeOut = 10000,
         alertTitle = alertTitle,
         coords = {
             x = pos.x,
             y = pos.y,
-            z = pos.z,
+            z = pos.z
         },
         details = {
             [1] = {
                 icon = '<i class="fas fa-passport"></i>',
-                detail = MyId .. ' | ' .. QBCore.Functions.GetPlayerData().charinfo.firstname .. ' ' .. QBCore.Functions.GetPlayerData().charinfo.lastname,
+                detail = MyId .. ' | ' .. QBCore.Functions.GetPlayerData().charinfo.firstname .. ' ' ..
+                    QBCore.Functions.GetPlayerData().charinfo.lastname
             },
             [2] = {
                 icon = '<i class="fas fa-globe-europe"></i>',
-                detail = streetLabel,
-            },
+                detail = streetLabel
+            }
         },
-        callSign = QBCore.Functions.GetPlayerData().metadata["callsign"],
+        callSign = QBCore.Functions.GetPlayerData().metadata["callsign"]
     }, true)
 end)
 
@@ -277,7 +300,7 @@ end)
 
 RegisterNetEvent('police:client:GunShotAlert')
 AddEventHandler('police:client:GunShotAlert', function(streetLabel, isAutomatic, fromVehicle, coords, vehicleInfo)
-    if PlayerJob.name == 'police' and onDuty then        
+    if PlayerJob.name == 'police' and onDuty then
         local msg = ""
         local blipSprite = 313
         local blipText = "Shots fired"
@@ -287,24 +310,24 @@ AddEventHandler('police:client:GunShotAlert', function(streetLabel, isAutomatic,
             MessageDetails = {
                 [1] = {
                     icon = '<i class="fas fa-car"></i>',
-                    detail = vehicleInfo.name,
+                    detail = vehicleInfo.name
                 },
                 [2] = {
                     icon = '<i class="fas fa-closed-captioning"></i>',
-                    detail = vehicleInfo.plate,
+                    detail = vehicleInfo.plate
                 },
                 [3] = {
                     icon = '<i class="fas fa-globe-europe"></i>',
-                    detail = streetLabel,
-                },
+                    detail = streetLabel
+                }
             }
         else
             blipText = "Shots fired"
             MessageDetails = {
                 [1] = {
                     icon = '<i class="fas fa-globe-europe"></i>',
-                    detail = streetLabel,
-                },
+                    detail = streetLabel
+                }
             }
         end
 
@@ -314,10 +337,10 @@ AddEventHandler('police:client:GunShotAlert', function(streetLabel, isAutomatic,
             coords = {
                 x = coords.x,
                 y = coords.y,
-                z = coords.z,
+                z = coords.z
             },
             details = MessageDetails,
-            callSign = QBCore.Functions.GetPlayerData().metadata["callsign"],
+            callSign = QBCore.Functions.GetPlayerData().metadata["callsign"]
         })
 
         PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
@@ -354,23 +377,23 @@ AddEventHandler('police:client:VehicleCall', function(pos, alertTitle, streetLab
             coords = {
                 x = pos.x,
                 y = pos.y,
-                z = pos.z,
+                z = pos.z
             },
             details = {
                 [1] = {
                     icon = '<i class="fas fa-car"></i>',
-                    detail = modelName,
+                    detail = modelName
                 },
                 [2] = {
                     icon = '<i class="fas fa-closed-captioning"></i>',
-                    detail = modelPlate,
+                    detail = modelPlate
                 },
                 [3] = {
                     icon = '<i class="fas fa-globe-europe"></i>',
-                    detail = streetLabel,
-                },
+                    detail = streetLabel
+                }
             },
-            callSign = QBCore.Functions.GetPlayerData().metadata["callsign"],
+            callSign = QBCore.Functions.GetPlayerData().metadata["callsign"]
         })
         PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
         local transG = 250
@@ -406,19 +429,19 @@ AddEventHandler('police:client:HouseRobberyCall', function(coords, msg, gender, 
             coords = {
                 x = coords.x,
                 y = coords.y,
-                z = coords.z,
+                z = coords.z
             },
             details = {
                 [1] = {
                     icon = '<i class="fas fa-venus-mars"></i>',
-                    detail = gender,
+                    detail = gender
                 },
                 [2] = {
                     icon = '<i class="fas fa-globe-europe"></i>',
-                    detail = streetLabel,
-                },
+                    detail = streetLabel
+                }
             },
-            callSign = QBCore.Functions.GetPlayerData().metadata["callsign"],
+            callSign = QBCore.Functions.GetPlayerData().metadata["callsign"]
         })
 
         PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
@@ -456,22 +479,22 @@ AddEventHandler('112:client:SendPoliceAlert', function(notifyType, data, blipSet
                 details = {
                     [1] = {
                         icon = '<i class="fas fa-video"></i>',
-                        detail = data.camId,
+                        detail = data.camId
                     },
                     [2] = {
                         icon = '<i class="fas fa-closed-captioning"></i>',
-                        detail = data.plate,
+                        detail = data.plate
                     },
                     [3] = {
                         icon = '<i class="fas fa-globe-europe"></i>',
-                        detail = data.streetLabel,
-                    },
+                        detail = data.streetLabel
+                    }
                 },
-                callSign = QBCore.Functions.GetPlayerData().metadata["callsign"],
+                callSign = QBCore.Functions.GetPlayerData().metadata["callsign"]
             })
             RadarSound()
         end
-    
+
         if blipSettings ~= nil then
             local transG = 250
             local blip = AddBlipForCoord(blipSettings.x, blipSettings.y, blipSettings.z)
@@ -507,10 +530,10 @@ AddEventHandler('police:client:PoliceAlertMessage', function(title, streetLabel,
             details = {
                 [1] = {
                     icon = '<i class="fas fa-globe-europe"></i>',
-                    detail = streetLabel,
-                },
+                    detail = streetLabel
+                }
             },
-            callSign = QBCore.Functions.GetPlayerData().metadata["callsign"],
+            callSign = QBCore.Functions.GetPlayerData().metadata["callsign"]
         })
 
         PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
@@ -521,7 +544,7 @@ AddEventHandler('police:client:PoliceAlertMessage', function(title, streetLabel,
         SetBlipAlpha(blip, transG)
         SetBlipAsShortRange(blip, false)
         BeginTextCommandSetBlipName('STRING')
-        AddTextComponentString("911 - "..title)
+        AddTextComponentString("911 - " .. title)
         EndTextCommandSetBlipName(blip)
         while transG ~= 0 do
             Wait(180 * 4)
@@ -539,8 +562,11 @@ end)
 RegisterNetEvent('police:server:SendEmergencyMessageCheck')
 AddEventHandler('police:server:SendEmergencyMessageCheck', function(MainPlayer, message, coords)
     local PlayerData = QBCore.Functions.GetPlayerData()
-    if ((PlayerData.job.name == "police" or PlayerData.job.name == "ambulance" or PlayerData.job.name == "doctor") and onDuty) then
-        TriggerEvent('chatMessage', "911 ALERT - " .. MainPlayer.PlayerData.charinfo.firstname .. " " .. MainPlayer.PlayerData.charinfo.lastname .. " ("..MainPlayer.PlayerData.source..")", "warning", message)
+    if ((PlayerData.job.name == "police" or PlayerData.job.name == "ambulance" or PlayerData.job.name == "doctor") and
+        onDuty) then
+        TriggerEvent('chatMessage',
+            "911 ALERT - " .. MainPlayer.PlayerData.charinfo.firstname .. " " .. MainPlayer.PlayerData.charinfo.lastname ..
+                " (" .. MainPlayer.PlayerData.source .. ")", "warning", message)
         TriggerEvent("police:client:EmergencySound")
         local transG = 250
         local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
@@ -586,14 +612,14 @@ AddEventHandler('police:client:SendToJail', function(time)
 end)
 
 function RadarSound()
-    PlaySoundFrontend( -1, "Beep_Green", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1 )
+    PlaySoundFrontend(-1, "Beep_Green", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1)
     Citizen.Wait(100)
-    PlaySoundFrontend( -1, "Beep_Red", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1 )
+    PlaySoundFrontend(-1, "Beep_Red", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1)
     Citizen.Wait(100)
-    PlaySoundFrontend( -1, "Beep_Green", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1 )
+    PlaySoundFrontend(-1, "Beep_Green", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1)
     Citizen.Wait(100)
-    PlaySoundFrontend( -1, "Beep_Red", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1 )
-    Citizen.Wait(100)   
+    PlaySoundFrontend(-1, "Beep_Red", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1)
+    Citizen.Wait(100)
 end
 
 function GetClosestPlayer()
@@ -602,7 +628,7 @@ function GetClosestPlayer()
     local closestPlayer = -1
     local coords = GetEntityCoords(PlayerPedId())
 
-    for i=1, #closestPlayers, 1 do
+    for i = 1, #closestPlayers, 1 do
         if closestPlayers[i] ~= PlayerId() then
             local pos = GetEntityCoords(GetPlayerPed(closestPlayers[i]))
             local distance = #(pos - coords)
@@ -612,12 +638,12 @@ function GetClosestPlayer()
                 closestDistance = distance
             end
         end
-	end
+    end
 
-	return closestPlayer, closestDistance
+    return closestPlayer, closestDistance
 end
 
-function DrawText3D(x,y,z, text)
+function DrawText3D(x, y, z, text)
     local onScreen, _x, _y = World3dToScreen2d(x, y, z)
     local p = GetGameplayCamCoords()
     local distance = #(p - vector3(x, y, z))
@@ -633,15 +659,15 @@ function DrawText3D(x,y,z, text)
         SetTextEntry("STRING")
         SetTextCentre(1)
         AddTextComponentString(text)
-        DrawText(_x,_y)
-        --local factor = (string.len(text)) / 370
-		--DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 0, 0, 0, 100)
-      end
-  end
+        DrawText(_x, _y)
+        -- local factor = (string.len(text)) / 370
+        -- DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 0, 0, 0, 100)
+    end
+end
 
 function loadAnimDict(dict)
     while (not HasAnimDictLoaded(dict)) do
         RequestAnimDict(dict)
         Citizen.Wait(10)
     end
-end 
+end
