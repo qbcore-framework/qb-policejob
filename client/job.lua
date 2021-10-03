70470416d7e93bf34b273a64433d5807361c7639
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-10-03 18:46:11 +0300

    support infinity onesync

--- client/job.lua
+++ client/job.lua
@@ -332,36 +332,79 @@ RegisterNUICallback('closeFingerprint', function()
     inFingerprint = false
 end)
 
-RegisterNetEvent('police:client:SendEmergencyMessage')
-AddEventHandler('police:client:SendEmergencyMessage', function(message)
-    local coords = GetEntityCoords(PlayerPedId())
-    
-    TriggerServerEvent("police:server:SendEmergencyMessage", coords, message)
-    TriggerEvent("police:client:CallAnim")
+RegisterNetEvent('police:client:createBlip')
+AddEventHandler('police:client:createBlip', function(type, coords, name, message, id, source)
+    PlayerJob = QBCore.Functions.GetPlayerData().job
+
+	if ((PlayerJob.name == "police" or PlayerJob.name == "ambulance") and PlayerJob.onduty) or (source == GetPlayerServerId(PlayerId())) then
+		local blip = AddBlipForCoord(coords)
+		SetBlipSprite(blip, 280)
+		SetBlipScale(blip, 0.8)
+		SetBlipColour(blip, typ == "police" and 1 or 3)
+		SetBlipAsShortRange(blip, false)
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString(type == "police" and "Police Call" or "Ambulance Call")
+		EndTextCommandSetBlipName(blip)
+
+		if type == "police" then
+            TriggerEvent('chat:addMessage', {template = '<div class="chat-message" style="background-color: rgba(59, 160, 255, 0.75);"><b>Emergency Call 911 #' .. id .. ' | ' .. name .. '</b> ' .. message .. '</div>'})
+		elseif type == "ems" then
+			TriggerEvent('chat:addMessage', {template = '<div class="chat-message" style="background-color: rgba(255, 59, 62, 0.75);"><b>Emergency Call 977 #' .. id .. ' | ' .. name .. '</b> ' .. message .. '</div>'})
+		end
+
+		TriggerEvent("InteractSound_CL:PlayOnOne", "demo", 0.4)
+		Citizen.Wait(60000)
+		RemoveBlip(blip)
+	end
 end)
+
+RegisterNetEvent('police:client:reply')
+AddEventHandler('police:client:reply', function(type, id, message,officer, source)
+	if ((PlayerJob.name == "police" or PlayerJob.name == "ambulance") and PlayerJob.onduty) or (source == GetPlayerServerId(PlayerId())) then
+		TriggerEvent('chat:addMessage', {template = '<div class="chat-message server"><b>Emergency Call ' .. (type == 'police' and '911' or '977') .. 'r #' .. id .. ' | ' .. officer .. '</b> ' .. message .. '</div>'})
+		TriggerEvent("InteractSound_CL:PlayOnOne", "demo", 0.4)
+	end
+end)
+
+RegisterNetEvent('police:client:called')
+AddEventHandler('police:client:called', function()
+	if QBCore.Functions.GetPlayerData().metadata["isdead"] or IsEntityPlayingAnim(PlayerPedId(), "cellphone@", "cellphone_call_listen_base", 3) or IsPedRagdoll(PlayerPedId()) then
+		return
+	end
+    RequestAnimDict("cellphone@")
+    while ( not HasAnimDictLoaded( "cellphone@" ) ) do
+        Wait(10)
+    end
+    TaskPlayAnim(PlayerPedId(), "cellphone@", "cellphone_call_listen_base", 2.0, 1.0, -1, 49, 0, 0, 0, 0)
+    attachPropPhone()
+    Wait(5000)
+    ClearPedTasks(PlayerPedId())
+    removeAttachedPropPhone()
+end)
+
+function removeAttachedPropPhone()
+	if DoesEntityExist(attachedPropPhone) then
+		DeleteEntity(attachedPropPhone)
+		attachedPropPhone = 0
+	end
+end
+
+function attachPropPhone()
+    removeAttachedPropPhone()
+	attachModelPhone = GetHashKey('prop_player_phone_01')
+	SetCurrentPedWeapon(PlayerPedId(), 0xA2719263)
+    local bone = GetPedBoneIndex(PlayerPedId(), 57005)
+	RequestModel(attachModelPhone)
+	while not HasModelLoaded(attachModelPhone) do
+		Citizen.Wait(100)
+    end
+	attachedPropPhone = CreateObject(attachModelPhone, 1.0, 1.0, 1.0, 1, 1, 0)
+	AttachEntityToEntity(attachedPropPhone, PlayerPedId(), bone, 0.14, 0.01, -0.02, 110.0, 120.0, -15.0, 1, 0, 0, 0, 2, 1)
+end
 
 RegisterNetEvent('police:client:EmergencySound')
 AddEventHandler('police:client:EmergencySound', function()
     PlaySound(-1, "Event_Start_Text", "GTAO_FM_Events_Soundset", 0, 0, 1)
-end)
-
-RegisterNetEvent('police:client:CallAnim')
-AddEventHandler('police:client:CallAnim', function()
-    local isCalling = true
-    local callCount = 5
-    loadAnimDict("cellphone@")   
-    TaskPlayAnim(PlayerPedId(), 'cellphone@', 'cellphone_call_listen_base', 3.0, -1, -1, 49, 0, false, false, false)
-    Citizen.Wait(1000)
-    Citizen.CreateThread(function()
-        while isCalling do
-            Citizen.Wait(1000)
-            callCount = callCount - 1
-            if callCount <= 0 then
-                isCalling = false
-                StopAnimTask(PlayerPedId(), 'cellphone@', 'cellphone_call_listen_base', 1.0)
-            end
-        end
-    end)
 end)
 
 RegisterNetEvent('police:client:ImpoundVehicle')
