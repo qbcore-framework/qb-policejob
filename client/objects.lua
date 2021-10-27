a60163af847fe1009a65d6c44bc161ef8fb4510f
Aljo003 <68874890+Aljo003@users.noreply.github.com>
2021-10-28 00:30:44 +0200

    Fix for spikes 3d text error

--- client/objects.lua
+++ client/objects.lua
@@ -49,6 +49,21 @@ function GetClosestSpike()
     ClosestSpike = current
 end
 
+local function DrawText3D(x, y, z, text)
+	SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(true)
+    AddTextComponentString(text)
+    SetDrawOrigin(x,y,z, 0)
+    DrawText(0.0, 0.0)
+    local factor = (string.len(text)) / 370
+    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
+    ClearDrawOrigin()
+end
+
 -- Events
 
 RegisterNetEvent('police:client:spawnCone', function()
