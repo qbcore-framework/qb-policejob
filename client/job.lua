9004c74db50b8bb0183fc1c36b109a015a0d0b9a
Jeremy <68263976+ansljeremy@users.noreply.github.com>
2021-11-01 16:26:44 -0400

    Translation
    Translated police abbreviation

--- client/job.lua
+++ client/job.lua
@@ -177,7 +177,7 @@ function TakeOutVehicle(vehicleInfo)
     local coords = Config.Locations["vehicle"][currentGarage]
     QBCore.Functions.SpawnVehicle(vehicleInfo, function(veh)
 	    SetCarItemsInfo()
-        SetVehicleNumberPlateText(veh, "PLZI"..tostring(math.random(1000, 9999)))
+        SetVehicleNumberPlateText(veh, "POL"..tostring(math.random(10000, 99999)))
         SetEntityHeading(veh, coords.w)
         exports['LegacyFuel']:SetFuel(veh, 100.0)
         closeMenuFull()
