ebdd9c2ee606c54e7fd361afcb0178ff22d11c67
mk3ext <me@mk3ext.dev>
2022-03-17 21:58:58 +0000

    Added convar

--- config.lua
+++ config.lua
@@ -14,7 +14,7 @@ Config.HandCuffItem = 'handcuffs'
 
 Config.LicenseRank = 2
 
-Config.UseTarget = false
+Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
 Config.Locations = {
     ["duty"] = {
         [1] = vector3(440.085, -974.924, 30.689),
