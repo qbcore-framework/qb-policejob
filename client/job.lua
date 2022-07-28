6eb0585594fddebebbe2fdbd08deda47e2171dea
Subham PRO <33375250+subhampro@users.noreply.github.com>
2022-07-28 10:14:06 +0530

    ISSUE ::  Everyone can access PD Evidence Stash ! FIXED !
    ISSUE ::  Everyone can access PD Evidence Stash ! FIXED ! He Must be Onduty too

--- client/job.lua
+++ client/job.lua
@@ -573,7 +573,7 @@ CreateThread(function()
     local evidenceCombo = ComboZone:Create(evidenceZones, {name = "evidenceCombo", debugPoly = false})
     evidenceCombo:onPlayerInOut(function(isPointInside)
         if isPointInside then
-            if onDuty then
+            if PlayerJob.name == "police" and onDuty then
                 local currentEvidence = 0
                 local pos = GetEntityCoords(PlayerPedId())
 
