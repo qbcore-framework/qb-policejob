56045abd31d45e3bd995dfeea05e2b1accecc96b
worapol.pan.buraphan <worapol.pan.buraphan@gmail.com>
2022-10-30 15:00:29 +0700

    Update th.lua

--- locales/th.lua
+++ locales/th.lua
@@ -184,7 +184,10 @@ local Translations = {
     },
 }
 
-Lang = Lang or Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'th' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
